cb1ebf6bc04551484e7ab5602659e165a4fde01e
zcr003 <zramsey1@cnm.edu>
2021-11-22 09:36:08 -0700

    Implementation of sorting the piles.

--- src/main/java/edu/cnm/deepdive/Trick.java
+++ src/main/java/edu/cnm/deepdive/Trick.java
@@ -1,11 +1,54 @@
 package edu.cnm.deepdive;
 
+import edu.cnm.deepdive.model.Card;
+import edu.cnm.deepdive.model.Rank;
+import edu.cnm.deepdive.model.Suit;
+import edu.cnm.deepdive.model.Suit.Color;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
 public class Trick {
 
-  //TODO Create a shuffled deck.
-  // TODO Sort the deck into 2 piles, according to the rules of the trick.
-  // TODO Swap a random number of cards between the 2 piles; take from the smaller of the 2 piles.
-  // TODO Sort the piles
-  // TODO Count & print the pile contents.
+  public static void main(String[] args) {
+    Random rng = new SecureRandom();
+    List<Card> deck = getDeck(rng);
+
+    List<Card> redPile = new ArrayList<>();
+    List<Card> blackPile = new ArrayList<>();
+   sortCards(deck, redPile, blackPile);
+    // TODO Swap a random number of cards between the 2 piles; take from the smaller of the 2 piles.
+    // TODO Sort the piles
+    // TODO Count & print the pile contents.
+
+  }
+
+  private static List<Card> getDeck(Random rng) {
+    List<Card> deck = new ArrayList<>();
+    for (Suit suit : Suit.values()) {
+      for (Rank rank : Rank.values()) {
+        deck.add(new Card(rank, suit));
+
+      }
+    }
+    Collections.shuffle(deck, rng);
+    return deck;
+  }
+
+  private static void sortCards(List<Card> deck, List<Card> redPile, List<Card> blackPile) {
+    for (Iterator<Card> iter = deck.iterator(); iter.hasNext(); ) {
+      Card selector = iter.next();
+      Card card = iter.next();
+      if (selector.getSuit().getColor() == Color.RED) {
+        redPile.add(card);
+      } else {
+        blackPile.add(card);
+      }
+    }
+  }
 
 }
